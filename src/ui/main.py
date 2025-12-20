c580db67b2f66015aba95e41faa8767480cc6f18
jggomez <juan.gomez01@gmail.com>
2025-12-20 16:52:45 -0600

    fix errors

--- src/ui/main.py
+++ src/ui/main.py
@@ -1,12 +1,11 @@
 import os
 import sys
 import time
-
+sys.path.insert(0, os.path.abspath(
+    os.path.join(os.path.dirname(__file__), "..")))
 import streamlit as st
 from backend import FacadeLoan
 from dotenv import load_dotenv
-
-sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
 
 
 load_dotenv()
